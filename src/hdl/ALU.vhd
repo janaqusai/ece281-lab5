fb74a1444913a632d5eb1a394a49cff1cb81f227
janaqusai <156135874+janaqusai@users.noreply.github.com>
2024-05-08 00:02:54 -0600

    All

--- src/hdl/ALU.vhd
+++ src/hdl/ALU.vhd
@@ -73,14 +73,29 @@ w_B(7 downto 0) <= i_B;
 w_add_res <= std_logic_vector(unsigned(w_A) + unsigned(w_B)) when (i_op = "000") else
              std_logic_vector(unsigned(w_A) - unsigned(w_B)) when (i_op = "100");
 
+w_and_res <= w_A and w_B when (i_op = "001") else
+             w_A nand w_B when (i_op = "101");
+
+w_or_res <= w_A or w_B when (i_op = "010") else
+            w_A nor w_B when (i_op = "110");
+
+w_shift_res <= std_logic_vector(shift_left(unsigned(w_A), to_integer(unsigned(w_B(2 downto 0))))) when (i_op = "011") else
+               std_logic_vector(shift_right(unsigned(w_A), to_integer(unsigned(w_B(2 downto 0))))) when (i_op = "111");
 
 w_result <= w_add_res when (i_op = "000") else
             w_add_res when (i_op = "100") else
+            w_and_res when (i_op = "001") else
+            w_and_res when (i_op = "101") else
+            w_or_res when (i_op = "010") else
+            w_or_res when (i_op = "110") else
+            w_shift_res when (i_op = "011") else
+            w_shift_res when (i_op = "111") else
             b"000000000";
 
 o_result <= w_result(7 downto 0);
 
-o_flags(2) <= '1' when (w_result(7) = '1');
+o_flags(2) <= '1' when (w_result(7) = '1') else
+              '0';
 
 o_flags(1) <= '1' when (w_result = b"000000000") else
               '0';
